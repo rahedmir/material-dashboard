3335a38844dbaa189ccb10f4083c4c7bcc51e445
Mir Rahed Uddin <rahedmir@gmail.com>
2020-06-05 09:43:42 +0530

    Initial commit

---
+++ app/src/main/java/rahedmir/github/materialdesigndashboard/MainActivity.java
@@ -0,0 +1,19 @@
+package rahedmir.github.materialdesigndashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
